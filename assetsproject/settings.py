fd04b72e826929d501f9d097c5565067a862e15c
Mellany Linhares <mellinhares6@gmail.com>
2025-06-23 19:41:58 -0300

    fix: typo

--- assetsproject/settings.py
+++ assetsproject/settings.py
@@ -159,7 +159,7 @@ DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = os.environ.get('EMAIL_HOST', 587)
+EMAIL_PORT = os.environ.get('EMAIL_PORT', 587)
 EMAIL_USE_TLS = os.environ.get('EMAIL_HOST', True)
 
 
