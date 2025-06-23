189767a2ebb99a466e7618406e27b56e8016a3ad
Mellany Linhares <mellinhares6@gmail.com>
2025-06-23 19:23:51 -0300

    feat: add models admin

--- assets/admin.py
+++ assets/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from assets.models import Asset, AssetPriceHistory
+
+
+@admin.register(Asset)
+class AssetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'tracking_frequency',)
+    search_fields = ('name', 'user_id',)
+
+
+@admin.register(AssetPriceHistory)
+class AssetPriceHistoryAdmin(admin.ModelAdmin):
+    list_display = ('asset', 'price', 'retrieved_at',)
+    search_fields = ('asset__name', 'asset_id',)
