e85dc133e243d86576f79c8c2031e52f4f5b4b2c
Mellany Linhares <mellinhares6@gmail.com>
2025-06-23 21:30:57 -0300

    feat: adjust auth templates

--- assets_auth/forms/forms.py
+++ assets_auth/forms/forms.py
@@ -25,20 +25,11 @@ class SignUpForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.layout = Layout(
-            Fieldset(
-                '',
-                'email',
-                'password1',
-                'password2',
-            ),
-            Submit('submit', 'Cadastrar', css_class='btn-login btn btn-block'),
-        )
 
     def clean(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
         if password1 and password2 and password1 != password2:
             self.add_error('password2', 'As senhas não são iguais.')
 
