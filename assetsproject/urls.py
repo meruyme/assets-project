0d0b2abbdc81bcc91215dda0bb08e99d04a84a3b
Mellany Linhares <mellinhares6@gmail.com>
2025-06-23 22:54:52 -0300

    feat: adjust urls

--- assetsproject/urls.py
+++ assetsproject/urls.py
@@ -1,8 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('assets_auth.urls', namespace='assets_auth')),
     path('assets/', include('assets.urls', namespace='assets')),
+    path('', RedirectView.as_view(url='auth/login/')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
