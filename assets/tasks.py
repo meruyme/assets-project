72d1b6116c1bbb8fd297bb6e364261bc54588764
Mellany Linhares <mellinhares6@gmail.com>
2025-06-24 10:42:05 -0300

    fix: send email only when asset intersect the limits

--- assets/tasks.py
+++ assets/tasks.py
@@ -30,8 +30,18 @@ def retrieve_asset(asset_id):
         market_time=asset_data['market_time'],
     )
 
+    last_asset_price = AssetPriceHistory.objects.filter(
+        asset_id=asset_id
+    ).exclude(
+        id=price_history.id
+    ).order_by('retrieved_at').values('price').last()
+
+    logger.info(f"Asset ID {asset_id}: Price created successfully. Asset price history ID: {price_history.id}")
+
     try:
-        if price_history.price > asset.upper_limit:
+        if (
+            (last_asset_price and last_asset_price['price'] <= asset.upper_limit) or not last_asset_price
+        ) and price_history.price > asset.upper_limit:
             email_message = (
                 f'O ativo {asset.name} cruzou o limite superior do túnel cadastrado, estando atualmente com o preço '
                 f'R${price_history.price}.'
@@ -42,8 +52,11 @@ def retrieve_asset(asset_id):
                 email_subject='Sugestão de venda de ativo',
                 email_message=email_message,
             )
+            logger.info(f"Asset ID {asset_id}: E-mail sent successfully - Message: {email_message}.")
 
-        elif price_history.price < asset.lower_limit:
+        elif (
+            (last_asset_price and last_asset_price['price'] >= asset.lower_limit) or not last_asset_price
+        ) and price_history.price < asset.lower_limit:
             email_message = (
                 f'O ativo {asset.name} cruzou o limite inferior do túnel cadastrado, estando atualmente com o preço '
                 f'R${price_history.price}.'
@@ -54,7 +67,6 @@ def retrieve_asset(asset_id):
                 email_subject='Sugestão de compra de ativo',
                 email_message=email_message,
             )
+            logger.info(f"Asset ID {asset_id}: E-mail sent successfully - Message: {email_message}.")
     except Exception as e:
         logger.error(f"Asset ID {asset_id}: Error sending e-mail: {str(e)}.")
-
-    logger.info(f"Asset ID {asset_id}: Price created successfully. Asset price history ID: {price_history.id}")
